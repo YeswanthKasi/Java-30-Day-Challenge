7a4ec0df6bb133b9704decee27c7e669bd6e0bc2
Yeswanth <kasireddiyeswanth29@gmail.com>
2025-06-25 15:06:14 +0530

    Day2: Learned few basics of Java like Variables,Keywords,Datatypes,typecasting

---
+++ Day2.java
@@ -0,0 +1,5 @@
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.println("Learned about: \nVariables\nKeywords\nDatatypes\nTypecasting\nSyntax\nType conversion Order");
+    }
+}
