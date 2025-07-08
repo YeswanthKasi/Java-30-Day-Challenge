9a2867ee50e23911805537669a2db4b3f8cd14a0
Yeswanth <kasireddiyeswanth29@gmail.com>
2025-07-08 13:34:59 +0530

    Day 2,3

--- Day2.java
+++ Day2.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Day2 {
     public static void main(String[] args) {
+        int a = 10;
         System.out.println("Learned about: \nVariables\nKeywords\nDatatypes\nTypecasting\nSyntax\nType conversion Order");
+        System.out.println(a);
+        System.out.println("Kasireddi Yeswanth");
     }
 }
+
---
+++ Day3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Day3 {
+    public static void main(String[] args) {
+        int l,b;
+        Scanner c = new Scanner(System.in);
+        l = c.nextInt();
+        b = c.nextInt();
+        System.out.println(l*b);
+    }
+}
