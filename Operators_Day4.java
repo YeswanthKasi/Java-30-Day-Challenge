4c2c80caa6670be4a49be17325f4e94f4cabc671
Yeswanth <kasireddiyeswanth29@gmail.com>
2025-07-08 16:35:52 +0530

    Day4

---
+++ Operators_Day4.java
@@ -0,0 +1,13 @@
+public class Operators_Day4 {
+    public static void main(String[] args) {
+        float a = 8;
+        float b= 3;
+        float c= a%b;
+        float d= a/b;
+        float e= a+b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+} 
+ 
